94b668ab77b685a2b24fb4fd30a4e3dc648e6829
Pramod Anandarao <panandarao@wisc.edu>
2025-07-05 03:53:17 -0500

    added Sphinx docs

---
+++ docs/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import importlib.metadata
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path('..').resolve()))
+
+project = 'GeoReport'
+copyright = '2025, Pramod Anandarao'
+author = 'Pramod Anandarao'
+release = importlib.metadata.version('georeport')
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
+    'sphinx_autodoc_typehints',
+]
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
---
+++ docs/reference.rst
@@ -0,0 +1,21 @@
+*********
+Reference
+*********
+
+GeoReport
+=========
+.. autoclass:: georeport.GeoReport
+    :members:
+    :undoc-members:
+
+CityEndpoint
+============
+.. autoclass:: georeport.CityEndpoint
+    :members:
+    :undoc-members:
+
+CityEndpoints
+=============
+.. autoclass:: georeport.CityEndpoints
+    :members:
+    :undoc-members:
