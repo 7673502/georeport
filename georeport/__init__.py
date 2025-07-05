626768e659348b50cb9fd679b2381a0666b3f3a9
Pramod Anandarao <panandarao@wisc.edu>
2025-07-05 01:53:57 -0500

    updated __init__.py so that version does not have to be manually updated

--- georeport/__init__.py
+++ georeport/__init__.py
@@ -1,3 +1,6 @@
-__version__ = '0.2.0'
+import importlib.metadata
+
+__version__ = importlib.metadata.version('georeport')
+
 from .georeport import GeoReport
 from .cityendpoint import CityEndpoints, CityEndpoint
